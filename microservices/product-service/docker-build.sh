deede590f5cf789ad178b5e31c3fb25efc08a8a0
Shuichi Machida <shuichi.machida@gmail.com>
2022-06-08 23:56:48 +0900

    updated

--- microservices/product-service/docker-build.sh
+++ microservices/product-service/docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t ms-demo-04-persistence-product-service .
+docker build -t ms-demo-04-persistence-product-service --platform linux/amd64 .
 docker images | grep product-service
--- microservices/recommendation-service/docker-build.sh
+++ microservices/recommendation-service/docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t ms-demo-04-persistence-recommendation-service .
+docker build -t ms-demo-04-persistence-recommendation-service --platform linux/amd64 .
 docker images | grep recommendation-service
